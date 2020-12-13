c0feb4c7aaccbc9c6e1052dd7b81defb3634e15d
mike-petrov <mis5116@yandex.ru>
2020-12-13 21:46:38 +0300

    add proxy

--- script/script/spiders/instagram.py
+++ script/script/spiders/instagram.py
@@ -1,9 +1,19 @@
 import scrapy
+API = '95413e818c49686b5d90a4bd8834bb54'
+
+def get_url(url):
+    payload = {'api_key': API, 'url': url}
+    proxy_url = 'http://api.scraperapi.com/?' + urlencode(payload)
+    return proxy_url
 
 class InstagramSpider(scrapy.Spider):
     name = 'instagram'
-    allowed_domains = ['instagram.com']
-    start_urls = ['https://www.instagram.com/{0}/'.format('underwaterstuffs')]
+    allowed_domains = ['api.scraperapi.com']
+    custom_settings = {'CONCURRENT_REQUESTS_PER_DOMAIN': 5}
+
+    def start_requests(self):
+        url = f'https://www.instagram.com/{0}/'.format('underwaterstuffs')
+        yield scrapy.Request(get_url(url), callback=self.parse)
 
     def parse(self, response):
         data={}
